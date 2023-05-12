7dac9872549d58b19a57144630ebca8000294de6
taslim <taslim@codemen.net>
2023-05-12 18:25:43 +0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Game/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
